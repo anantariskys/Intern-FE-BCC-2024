20ee40a0892e609c1627943c5f18b0e1a7ebe02e
anantariskys <gamersindo555@gmail.com>
2024-03-13 21:57:21 +0700

    fix footer button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -211,7 +211,7 @@ const Home = () => {
         <section className="w-full bg-Primary-Purple py-9 font-Poppins flex flex-col items-center rounded-t-3xl gap-8">
           <p className="w-1/5 rounded-full aspect-square bg-[#FFFDE3] flex justify-center items-center text-4xl bg-opacity-50">🙆‍♀️</p>
           <p className="text-sm font-normal text-Primary-White w-3/5 text-center">kunjungi halaman komunitas dan tingkatkan engagement bisnis dan jasamu!</p>
-          <button className="px-4 py-2 btn text-base bg-Primary-White text-Primary-Blue">Jelajahi Komunitas BRAW!</button>
+          <button className="px-4 py-2 btn text-base bg-Primary-White hover:bg-Secondary-LightTeal duration-300 ease-in-out active:bg-Secondary-LightTeal text-Primary-Blue">Jelajahi Komunitas BRAW!</button>
         </section>
       </main>
 
